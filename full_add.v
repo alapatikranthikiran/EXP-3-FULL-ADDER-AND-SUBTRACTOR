219e4b57d7594642638825b42fb849f5f7fef967
ALAPATI KRANTHI KIRAN <160647575+alapatikranthikiran@users.noreply.github.com>
2025-10-15 18:04:49 +0530

    Add files via upload

---
+++ full_add.v
@@ -0,0 +1,11 @@
+// Full Adder in Verilog
+module full_add (
+    input  wire a, b, cin,   // Inputs
+    output wire sum, carry   // Outputs
+);
+
+    // Logic equations
+    assign sum   = a ^ b ^ cin;                  // XOR for sum
+    assign carry = (a & b) | (b & cin) | (a & cin); // Majority function for carry
+
+endmodule
